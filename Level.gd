823d2789f2792b85be96fc1a1b6622d3d45801cf
Mason Ray Hanna III <mhanna0000@gmail.com>
2022-12-05 15:05:35 -0500

    fixed bug where both paths were configured.

--- Level.gd
+++ Level.gd
@@ -10,11 +10,14 @@ onready var enemy_path = $Path2D
 # Called when the node enters the scene tree for the first time.
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_down"):
-		
 		can_select = !can_select
 		enemy_path2.selectable = !enemy_path2.selectable
 		enemy_path.selectable = !enemy_path.selectable
-		print("Select now!", can_select)
+		#enemy_path.selected = !enemy_path.selected
+		#enemy_path2.selected = !enemy_path2.selected
+		print("Select:", can_select)
+		print(enemy_path.curve.get_point_count(), enemy_path.selected)
+		print(enemy_path2.curve.get_point_count(), enemy_path2.selected)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- path_draw.gd
+++ path_draw.gd
@@ -1,17 +1,17 @@
 extends Path2D
-var selectable = false
-var selected = false
-var changed = false
 onready var pathFollow = $PathFollow2D
 export var traverseTime = 5
 export var activeTime = 0
 var pathLength = 0
 var dir = 1
-onready var path = self
+#onready var path = self
+#DEBUG USE VARIABLES
 var idx = 0
+var selectable = false
+var selected = false
+var changed = false
 var unselected_color = Color.white
 var selected_color = Color.red
-var dummy_pos = Vector2(50,50)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Hit down arrow key to make paths selectable.")
@@ -22,8 +22,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if changed:
-		pathLength = self.curve.get_baked_length()
 		changed = false
+		pathLength = self.curve.get_baked_length()
+		"""var p_s = PackedScene.new()
+		var name = "res://Paths/" + self.get_name() + ".tscn"
+		p_s.pack(get_tree().get_current_scene().get_node(self.get_path()))
+		ResourceSaver.save(name, p_s) 
+		p_s = load(name)
+		var new_child = p_s.instance()
+		add_child(new_child)
+		
+		new_child.set_owner(self)"""
 	if (activeTime >= traverseTime):
 		dir = -1
 	if (activeTime <= 0):
@@ -33,28 +42,34 @@ func _process(_delta):
 
 
 func _input(event):
-	if selectable:
+	if self.selectable:
 		if event is InputEventMouseButton:
-			if event.position.distance_to(self.position) < 50:
-				selected = true
-			if selected:
+			if self.selected:
 				if event.button_index == BUTTON_LEFT && event.is_pressed():
-					for p in path.curve.get_point_count():
-						if path.curve.get_point_position(p).distance_to(event.position) < 100:
+					for p in self.curve.get_point_count():
+						if self.curve.get_point_position(p).distance_to(event.position) < 100:
 							idx = p
 							update()
-						else:
-							path.curve.add_point(Vector2(event.position.x - self.position.x, event.position.y - self.position.y))
-							idx = path.curve.get_point_count()-1
-							changed = true
-							update()
 						#SELECT HANDLES
-						var a = path.curve.get_point_position(p) - path.curve.get_point_out(p)
-						var b = path.curve.get_point_position(p) - path.curve.get_point_in(p)
+						var a = self.curve.get_point_position(p) - self.curve.get_point_out(p)
+						var b = self.curve.get_point_position(p) - self.curve.get_point_in(p)
 						if a.distance_to(event.position) < 10:
 							idx = p
 						if b.distance_to(event.position) < 10:
 							idx = p
+					if idx == 0:
+						self.curve.add_point(Vector2(event.position.x - self.position.x, event.position.y - self.position.y))
+						idx = self.curve.get_point_count()-1
+						self.changed = true
+						update()
+			if event.position.distance_to(self.position) < 50:
+				self.selected = true
+				update()
+	else:
+		if self.selected: 
+			self.selected = false
+			update()
+	idx = 0
 
 
 #DRAW
@@ -64,12 +79,12 @@ func _draw():
 		c = selected_color
 	else:
 		c = unselected_color
-	draw_polyline(path.curve.get_baked_points(), c, 1, true)
-	for p in path.curve.get_point_count():
-		if p != 0 and p != path.curve.get_point_count() - 1:
-			var a = path.curve.get_point_position(p) - path.curve.get_point_in(p) * -1
-			var b = path.curve.get_point_position(p) - path.curve.get_point_out(p) * -1
-			var cp = path.curve.get_point_position(p)
+	draw_polyline(self.curve.get_baked_points(), c, 1, true)
+	for p in self.curve.get_point_count():
+		if p != 0 and p != self.curve.get_point_count() - 1:
+			var a = self.curve.get_point_position(p) - self.curve.get_point_in(p) * -1
+			var b = self.curve.get_point_position(p) - self.curve.get_point_out(p) * -1
+			var cp = self.curve.get_point_position(p)
 			draw_polyline(PoolVector2Array([a, cp]), c, 1, true)
 			draw_polyline(PoolVector2Array([b, cp]), c, 1, true)
 	update()
